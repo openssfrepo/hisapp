27842fe1f37791d2ab6c33e477a55eeeaf7bed71
openssfrepo <sonnet.amit@gmail.com>
2020-05-04 06:24:04 +0900

    hisapi_update

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,74 +1,73 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  View, FlatList, Text, Alert
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
 } from 'react-native';
-import {
-  Avatar,
-} from 'react-native-elements';
+import {Card} from '../Custom/Card';
+import {Avatar} from 'react-native-elements';
 import HisApi from '../Common/HisApi';
-import Card from '../Custom/Card';
-
 
 export default class Contact extends Component {
-  constructor() {
-    super();
-    this._loginUser = null;
-    this._ContactList = []
+  _contactList = [];
+  constructor(props) {
+    super(props);
+
     this.state = {
-      contactList: [],
-      isContactLoading: false
-    }
+      dataSource: [],
+    };
   }
-  async componentDidMount() {
-    var list = []
-    try{
-      
-     list = this.getAllUser()
-    } catch(e){
-      throw (e)
-    }
-    //   Alert.alert('data test', JSON.stringify(list))
-    //   // this.setState({
-    //   //   contactList: JSON.stringify(list),
-    //   //   isContactLoading: true
-    //   // })
-  }
-  getAllUser(){
-    return fetch('http://127.0.0.1:4001/getAllUser')
-      .then((response) => response.json())
-      .then((json) => {
-        return json;
-      })
-      .catch((error) => {
-        console.error(error)
-      });
+
+  componentDidMount() {
+    HisApi.getAllUser()
+    const list = HisApi.getLoginInfo()
+    this.setState({
+      dataSource: list
+    })
+
   }
   render() {
     return (
-      // <FlatList
-      //   data={this.state.contactList}
-      //   renderItem={this._renderContactList.bind(this)}
-      //   keyExtractor={(item, index) => index}
-      // />
-      <Text>data</Text>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={item => this._renderContactList(item)}
+          keyExtractor={item => item.id.toString()}
+        />
+      </SafeAreaView>
     );
   }
-  _renderContactList({ item, index }) {
-
-    return (
-      <View>
-        <Card>
+  _renderContactList = data => (
+    <TouchableOpacity style={styles.item}>
+      <Card>
+        <View
+          style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start'}}>
           <Avatar
             size="small"
             rounded
-            title="A"
-            onPress={() => console.log("works")}
+            title={data.item.Name.length > 1 ? data.item.Name.substring(0,2) : data.item.Name.substring(0,1)}
+            onPress={() => console.log('Works!')}
             activeOpacity={0.7}
           />
-          <Text>{item.Name}</Text>
-        </Card>
-      </View>
-    )
-  }
-
+          <Text style={{margin: 5, fontSize: 20}}>{data.item.Name}</Text>
+        </View>
+      </Card>
+    </TouchableOpacity>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 5,
+  },
+  item: {
+    // marginVertical: 3,
+    // marginHorizontal: 3,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
