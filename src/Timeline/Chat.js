29b5fb07eb21f6fdf97981594d134c03e83c6ebe
openssfrepo <sonnet.amit@gmail.com>
2020-11-03 03:51:05 +0900

    controller for custom sql added

--- src/Timeline/Chat.js
+++ src/Timeline/Chat.js
@@ -1,23 +1,81 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-//import { Card, Button, Icon } from 'react-native-elements'
-import {Card} from '../Custom/Card';
+import {View, Text, StyleSheet, TextInput, Button, KeyboardAvoidingView} from 'react-native';
+import io from 'socket.io-client';
 export default class Chat extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatMessage: '',
+      chatMessages: [],
+    };
+  }
+  componentDidMount() {
+    this.socket = io('http://127.0.0.1:4001');
+    this.socket.on('chat message', msg => {
+      this.setState({chatMessages: [...this.state.chatMessages, msg]});
+    });
   }
   render() {
+    const chatMessages = this.state.chatMessages.map(chatMessage => (
+      <Text style={{borderWidth: 2, top: 500}}>{chatMessage}</Text>
+    ));
     return (
-      <View>
-        <Card>
-          <Text>Any content you want to include on the card</Text>
-          <Text>More content that you want on the card</Text>
-        </Card>
-        <Card>
-          <Text>Any content you want to include on the card</Text>
-          <Text>More content that you want on the card</Text>
-        </Card>
+      // <View style={styles.container}>
+      //   {chatMessages}
+      //   <TextInput
+      //     style={{height: 40, borderWidth: 2, top: 600}}
+      //     autoCorrect={false}
+      //     value={this.state.chatMessage}
+      //     onSubmitEditing={() => this.submitChatMessage()}
+      //     onChangeText={chatMessage => {
+      //       this.setState({chatMessage});
+      //     }}
+      //   />
+      // </View>
+      <View style={styles.container}>
+      {chatMessages}
+        <KeyboardAvoidingView
+          style={{position: 'absolute', left: 0, right: 0, bottom: 0}}
+          behavior="position">
+          <TextInput
+            style={styles.input}
+            autoCorrect={false}
+            value={this.state.chatMessage}
+            onSubmitEditing={() => this.submitChatMessage()}
+            onChangeText={chatMessage => {
+              this.setState({chatMessage});
+            }}
+            underlineColorAndroid="transparent"
+          />
+          <Button onPress={() => this.submitChatMessage()} title="SEND" />
+        </KeyboardAvoidingView>
       </View>
     );
   }
+  submitChatMessage() {
+    this.socket.emit('chat message', this.state.chatMessage);
+    this.setState({chatMessage: ''});
+  }
 }
+// const styles = StyleSheet.create({
+//   container: {
+//     height: 400,
+//     flex: 1,
+//     backgroundColor: '#F5FCFF',
+//   },
+// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    position: 'relative',
+    bottom: 0,
+  },
+  input: {
+    backgroundColor: 'red',
+    width: '100%',
+    height: 40,
+    color: '#ffffff',
+    position: 'absolute', left: 0, right: 0, bottom: 0
+  },
+});
--- src/Timeline/QandA.js
+++ src/Timeline/QandA.js
@@ -1,22 +1,59 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {Card} from '../Custom/Card';
+import io from 'socket.io-client';
+import { GiftedChat, InputToolbar } from 'react-native-gifted-chat';
 export default class QandA extends Component {
   constructor() {
     super();
+    this.state = {
+      chatMessage: '',
+      giftedMessages: [],
+      chatMessages: [],
+    };
+  }
+  componentDidMount() {
+    this.socket = io('http://127.0.0.1:4001');
+    this.socket.on('chat message', msg => {
+      this.setState({giftedMessages: [...this.state.giftedMessages,{
+        _id:1,
+        text:msg,
+        createdAt:new  Date(),
+        user:{
+          _id: 1,
+          name: 'Aaron',
+          avatar: 'https://placeimg.com/150/150/any',
+        }
+        }]});
+    });
   }
   render() {
-    return (
-      <View>
-        <Card>
-          <Text>Any content you want to include on the card</Text>
-          <Text>More content that you want on the card</Text>
-        </Card>
-        <Card>
-          <Text>Any content you want to include on the card</Text>
-          <Text>More content that you want on the card</Text>
-        </Card>
-      </View>
+    return(
+      <GiftedChat
+      messages= {this.state.giftedMessages}
+      alignTop
+      alwaysShowSend
+      scrollToBottom
+      showUserAvatar
+      renderAvatarOnTop
+      renderUsernameOnMessage
+      onInputTextChanged={chatMessage => {
+              this.setState({chatMessage});
+            }}
+      onSend={()=>this.submitChatMessage()}
+      bottomOffset={26}
+      onPressAvatar={console.log}
+      isCustomViewBottom
+      ></GiftedChat>
     );
+  }
+  submitChatMessage() {
+    this.setState({giftedMessages: [...this.state.giftedMessages, {
+      _id:2,
+      text:this.state.chatMessage,
+      createdAt:new  Date(),
+      }]});
+    this.socket.emit('chat message', this.state.chatMessage);
+    //this.setState({chatMessage: ''});
   }
 }
