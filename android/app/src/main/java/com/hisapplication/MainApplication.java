273fe123f0face86cfcf44fa6c044faa0b0e0c57
openssfrepo <sonnet.amit@gmail.com>
2020-12-20 17:01:20 +0900

    android compitable issue fixed

--- android/app/src/main/java/com/hisapplication/MainApplication.java
+++ android/app/src/main/java/com/hisapplication/MainApplication.java
@@ -4,12 +4,13 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
+//import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          new VectorIconsPackage();
           return packages;
         }
 
